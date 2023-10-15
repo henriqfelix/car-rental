cd4821ba4518939d76ead61f5f181a1a28489d4c
henriqfelix <henriquefelix1821@gmail.com>
2023-10-15 20:06:49 -0300

    implementando lógica para carregar as imagens dos carros

--- src/components/Cars.tsx
+++ src/components/Cars.tsx
@@ -1,10 +1,24 @@
 "use client";
 
-import { MouseEvent } from "react";
+import kwid from "../assets/images/kwid.png";
+import p208 from "../assets/images/208.png";
+import cronos from "../assets/images/cronos.png";
+import pulse from "../assets/images/pulse.png";
+import tcross from "../assets/images/tcross.png";
+import jetta from "../assets/images/jetta.png";
+import a3 from "../assets/images/a3.png";
+
+import { MouseEvent, useState } from "react";
 import carList from "../json/Carros.json";
+
+import Image, { StaticImageData } from "next/image";
 
 export default function Cars() {
   let cars: React.JSX.Element[] = [];
+
+  const [carImage, setCarImage] = useState(
+    <Image src={kwid} alt={"Kwid"} width={500}></Image>
+  );
 
   for (const car of carList) {
     cars.push(
@@ -22,6 +36,7 @@ export default function Cars() {
 
   const chooseCar = (e: React.MouseEvent<HTMLElement>) => {
     removeSelected();
+    LoadImage(e.currentTarget.innerHTML);
 
     e.currentTarget.classList.add("car-selected");
   };
@@ -29,19 +44,51 @@ export default function Cars() {
   const removeSelected = () => {
     const buttons = document.querySelectorAll(".car-button");
     buttons.forEach((button) => {
-      console.log(button.classList.remove("car-selected"));
+      button.classList.remove("car-selected");
     });
+  };
+
+  const LoadImage = (carName: string) => {
+    let path: StaticImageData = kwid;
+
+    switch (carName) {
+      case "Renault Kwid":
+        path = kwid;
+        break;
+      case "Peugeot 208":
+        path = p208;
+        break;
+      case "Fiat Cronos":
+        path = cronos;
+        break;
+      case "Fiat Pulse":
+        path = pulse;
+        break;
+      case "Volkswagen T-Cross":
+        path = tcross;
+        break;
+      case "Volkswagen Jetta":
+        path = jetta;
+        break;
+      case "Audi A3 Sportback":
+        path = a3;
+        break;
+      default:
+        break;
+    }
+    console.log("load: ", carName, path.src);
+    setCarImage(<Image src={path} alt={carName} width={500}></Image>);
   };
 
   return (
     <section className="w-10/12 mx-auto pt-20 h-48">
       <div className="center flex-col">
-        <h1 className="big-title mt-2">Veja nossa frota</h1>
+        <h1 className="big-title mt-2">Conheça nossa frota</h1>
         <span className="sub-title">Temos carros para todos os gostos</span>
       </div>
       <div className="grid grid-cols-12 h-96 mt-20 gap-4">
         <div className="col-span-3 flex flex-col gap-1">{cars}</div>
-        <div className="bg-slate-200 col-span-6"></div>
+        <div className="col-span-6 center">{carImage}</div>
         <div className="bg-slate-300 col-span-3"></div>
       </div>
     </section>
